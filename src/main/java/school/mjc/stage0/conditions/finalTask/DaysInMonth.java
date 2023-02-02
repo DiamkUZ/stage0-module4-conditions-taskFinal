b5629f9c0002ac7344db9fb3526b9932e7336d8e
DiamkUZ <diamkms@yandex.ru>
2023-02-02 14:54:28 +0300

    all done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,36 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        if (year < 0 || (month < 0 && month > 12)) {
+            System.out.println("invalid date");
+            return;
+        }
+
+        boolean isYearLeap;
+        if (year % 400 == 0) {
+            isYearLeap = true;
+        } else {
+            if (year % 100 == 0) {
+                isYearLeap = false;
+            } else if (year % 4 == 0) {
+                isYearLeap = true;
+
+            } else {
+                isYearLeap = false;
+            }
+        }
+
+       switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> {
+                System.out.println(31);
+            }
+            case 4, 6, 9, 11 -> {
+                System.out.println(30);
+            }
+            case 2 -> {
+                System.out.println(isYearLeap ? 29 : 28);
+            }
+            default -> System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if ((firstSide > 0 && secondSide > 0 && thirdSide >0) &&
+               // AB + BC > AC, AC + BC > AB, AB + AC > BC
+               // AB - firstSide
+                //  BC - secondSide
+                //  AC - thirdSide
 
+                (firstSide + secondSide > thirdSide && thirdSide + secondSide > firstSide && firstSide + thirdSide > secondSide)) {
+            System.out.println("this is a valid triangle");
+        } else System.out.println("it's not a triangle");
     }
 }
